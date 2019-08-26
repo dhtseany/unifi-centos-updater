2c98ab3eecab09a490241a71f496b4dbd2ecd3c7
Sean Snell <snellsg@gmail.com>
2019-08-26 15:42:43 -0400

    Initial Commit

---
+++ unifi-update.sh
@@ -0,0 +1,133 @@
+#!/bin/bash
+clear
+
+# Misc Variables
+BACKUP_DIR=/opt/UniFi/data
+BACKUP_FILE=/root/backup.tar.gz
+
+# PKG_VER=5.10.25
+PKG_VER=5.11.39
+
+runCommand=$1
+
+if [[ ("$runCommand" == "repair") ]];
+	then
+		systemctl stop unifi
+		systemctl stop mongod
+		#/usr/bin/mongod --dbpath /opt/UniFi/data/db --port 27117 --unixSocketPrefix /opt/UniFi/run/ --logappend --logpath /opt/UniFi/logs/mongod.log --nohttpinterface --bind_ip 127.0.0.1
+		mongod --dbpath=/opt/UniFi/data/db --smallfiles --logpath /opt/UniFi/logs/server.log --repair
+		#chown -R ubnt:ubnt /opt/Unifi/data
+		chown -R ubnt:ubnt /opt/UniFi/data/
+		systemctl start mongod
+		systemctl start unifi
+fi
+exit 0
+
+#manualURL=$1
+#if [ -z $manualURL ];
+#	then
+#		PKG_URL="https://dl.ubnt.com/unifi/${PKG_VER}/UniFi.unix.zip"
+#		echo "[PKG_URL] $PKG_URL"
+#		# exit 0
+#	else
+#		PKG_URL="manualURL"
+#		echo "[PKG_URL] $PKG_URL"
+#		# exit 0
+#fi
+
+TMP_DIR=/tmp/unifi-updater
+
+# Create tmp_dir
+mkdir -p /tmp/unifi-updater
+
+# Check if existing backup file exists
+if [ -e $BACKUP_FILE ]; 
+    then
+        echo "You have chosen to launch the installer but I've detected an existing backup."
+        read -e -p "Shall I remove the existing backup file? [N/y] : " KILL_BACKUP
+
+        if [[ ("$KILL_BACKUP" == "y" || "$KILL_BACKUP" == "Y") ]]; 
+            then
+                clear
+                rm $BACKUP_FILE
+            else
+                clear
+                echo "User to chose to keep old backup."
+                mv $BACKUP_FILE $BACKUP_FILE.bak
+        fi
+
+    else
+        echo "No backup files found. Proceeding."
+fi
+
+# stop the unifi service
+echo "Stopping the Unifi service..."
+systemctl stop unifi
+
+# take a backup of the data directory
+echo "Creating a tar.gz backup of $BACKUP_DIR to $BACKUP_FILE, please wait..."
+START_DATE=$(date)
+echo "Started at: $START_DATE"
+tar zcf $BACKUP_FILE $BACKUP_DIR
+END_DATE=$(date)
+echo "Ended at: $END_DATE"
+#tar zcf - /opt/Unifi/data | (pv -p --timer --rate --bytes > $BACKUP_FILE)
+
+
+
+# cd to tmp_dir and fetch latest installer
+cd $TMP_DIR
+
+if [ -f "$TMP_DIR/Unifi.unix.zip" ];
+	then
+		echo "[Yes] $TMP_DIR/Unifi.unix.zip"
+		rm $TMP_DIR/Unifi.unix.zip
+		read -e -p "Is that file actually gone? " readFileGone
+		if [ "$readFileGone" = "y" ]; 
+	                then
+				echo "[great!]"
+			else
+				echo "[FAIL] User said the task failed. Exiting."
+				exit 1
+		fi
+	else
+            	echo "[No] $TMP_DIR/Unifi.unix.zip"
+                read -e -p "Is that file actually gone? " readFileGone
+                if [ "$readFileGone" = "y" ];
+                        then
+                            	echo "[great!]"
+                        else
+                            	echo "[FAIL] User said the task failed. Exiting."
+                                exit 1
+                fi
+
+fi
+
+echo ["Fetching] $PKG_URL"
+#wget https://dl.ubnt.com/unifi/${PKG_VER}/UniFi.unix.zip
+wget $PKG_URL
+
+# Unzip the download into place
+echo "Unzipping UniFi.unix.zip"
+unzip -qo UniFi.unix.zip -d /opt
+
+# Take ownership of the unzipped dir
+# echo "Taking ownership of the updated Unifi directory"
+# chown -R ubnt:ubnt /opt/UniFi
+
+# Untar the backup into place
+echo "Restoring $BACKUP_FILE to $BACKUP_DIR"
+# tar xzf $BACKUP_FILE -C $BACKUP_DIR
+tar xzf $BACKUP_FILE -C /
+
+# Take ownership of the unzipped dir
+echo "Taking ownership of the updated Unifi directory"
+chown -R ubnt:ubnt /opt/UniFi
+
+# Start the Unifi service
+echo "Starting to Unifi service..."
+systemctl start unifi
+
+# Complete
+echo "Process is completed."
+exit 0
