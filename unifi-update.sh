e69efb24585dec2a30e18eeccebf0996d8f9410b
Sean Snell <snellsg@gmail.com>
2020-09-18 10:49:00 -0400

    Work fixes.

--- unifi-update.sh
+++ unifi-update.sh
@@ -43,7 +43,10 @@ if [ $runCommand = "install" ]
 	then
 		if [ -z $runOption ]
 			then
-				echo "[ERROR] You have chosen to install to a new system however you have not specified a back to restore from."
+				echo "[ERROR] You have chosen to install to a new system however you have not specified a backup file to restore from."
+				echo "Usage:"
+				echo "$ sudo ./unifi-update.sh install /path/to/backup/file.tar.gz"
+				echo ""
 				echo "If you want to run a new install without restoring an existing config use:"
 				echo "$ sudo ./unifi-update.sh install fresh"
 				exit 1
